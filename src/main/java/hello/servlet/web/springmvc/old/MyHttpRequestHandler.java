3c88bb80c1a6d8a9068c4c5fa9fc432a5b05b5f2
jinsb1995 <jinsb1995@gmail.com>
2021-09-21 13:50:16 +0900

    handlerMapping, handlerAdapter

--- src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
+++ src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -8,13 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component("/springmvc/request-handler")
+@Component("/springmvc-/request-handler")
 public class MyHttpRequestHandler implements HttpRequestHandler {
+
+    // 핸들러 매핑은 RequestMapping 이나 BeanNameUrlMapping을 통해서 핸들러를 찾고
+    // 핸들러 어댑터가 Implements를 찾아서 우리가 만든걸 찾아준다.
 
 
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // HttpRequestHandler를 찾아주는 handle은 dispatcherServlet이 불러주는것이다.
-        System.out.println("MyHttpRequestHandler.handleRequest");
+
+        System.out.println("MyHttpRequestHandler  ================== MyHttpRequestHandler.handleRequest");
+
     }
 }
--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -7,12 +7,25 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// 스프링 빈의 이름을 url패턴에 맞춘 것이다.
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
 
+    // 핸들러 매핑
+    // 핸들러 매핑에서 이 컨트롤러를 찾을 수 있어야 한다.
+    // 스프링 빈의 이름으로 핸들러를 찾을 수 있는 핸들러 매핑이 필요하다.   ex) @Component("/springmvc/old-controller")  -> BeanNameUrlHandlerMapping
+
+
+    // 핸들러 어댑터
+    // 핸들러 매핑을 통해서 찾은 핸들러를 실행할 수 있는 핸들러 어댑터가 필요하다.
+    // Controller 인터페이스를 실행할 수 있는 핸들러 어댑터를 찾고 실행해야 한다.  ex) SimpleControllerHandlerAdapter
+
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("OldController.handleRequest");
+
+        System.out.println("OldController ================  OldController.handleRequest");
+
         return null;
     }
 }
